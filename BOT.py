b6a0cba429b0a64048577bff79620c99aaf2768d
Aul16 <39156836+Aul16@users.noreply.github.com>
2019-06-01 15:42:29 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -59,4 +59,4 @@ async def zenyatta(ctx):
     	"https://www.youtube.com/watch?v=vZAkzlPPx8I&")
 
 
-bot.run("NTgzOTMzMjI4MDA4NzM0NzIw.XPDtaA.PjN_7nL_GhNQz3blwAlFk-y02Lo")
+bot.run(os.environ['BOT_TOKEN'])
